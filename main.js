5c35a4ed7c36f819a1f34988dbb88cddca2b10b1
whiteShadow <elbedawey11@gmail.com>
2026-02-26 22:30:57 +0200

    try to

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('ASH Digital Hub Initialized');
-
+    // test
     // Multi-language support
     const translations = {
         ar: {
